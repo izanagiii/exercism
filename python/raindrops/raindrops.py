a7d69a720d94015adfc84dfc8292fad72a7227f3
izanagiii <im.izanagi@outlook.com>
2020-10-08 12:45:08 +0330

    1st solu to raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,15 @@
 def convert(number):
-    print("Hello")
+    result = ""
+    if number % 3 ==  0:
+        result += "Pling"
+    
+    if number % 5 == 0:
+        result += "Plang"
+        
+    if number % 7 == 0:
+        result += "Plong"
+    
+    if number % 3 !=  0 and  number % 5 != 0  and  number % 7  != 0:
+        result = str(number)
+    
+    return result
